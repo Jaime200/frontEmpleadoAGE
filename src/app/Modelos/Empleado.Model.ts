cdd00f45bf8d8dd7cb9486b3c2491ea80a9cc131
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-03 13:04:41 -0600

    Modelo de empleado

---
+++ src/app/Modelos/Empleado.Model.ts
@@ -0,0 +1,21 @@
+export  class EmpleadoModel {
+
+        public DPI              : number;
+        public PRIMER_NOMBRE    :string;
+        public SEGUNDO_NOMBRE   :string;
+        public PRIMER_APELLIDO  :string;
+        public SEGUNDO_APELLIDO :string;
+        public APELLIDO_CASADA  :string;
+        public ESTADO_CIVIL     :string;
+        public SEXO             :number;
+        public NIT              :number;
+        public AFILIACION_IGSS  :string;
+        public IRTRA            :string;
+        public PASAPORTE        :string;
+    constructor(        
+        
+    ){
+        
+    }
+    
+}
