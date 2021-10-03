1d50dd67c21c5485e77eee48d1455a939d92308e
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-03 17:35:50 -0600

    Se agrega modelo de sueldo empleado

---
+++ src/app/Modelos/Sueldo.Model.ts
@@ -0,0 +1,14 @@
+export  class SueldoModel {
+
+    public ID           : number;
+    public DPI          :number;
+    public SUELDO_BASE  : number;
+    public BONIFICACION : number;
+
+constructor(        
+    
+){
+    
+}
+
+}
