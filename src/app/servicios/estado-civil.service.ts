1011b30dc40e00c3f929a434d53a9567614f7692
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-03 10:30:01 -0600

    Se crearon servicios y parametrizacion de enviroments

--- src/app/servicios/estado-civil.service.ts
+++ src/app/servicios/estado-civil.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class EstadoCivilService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient,
+  ) { }
+
+  ObtenerBancos(){
+    let url= `${environment.URL_SERVICIOS}/BANCOS`
+   return  this.http.get(url)
+  }
 }
