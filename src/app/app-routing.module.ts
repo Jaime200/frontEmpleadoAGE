bfc84e1a044bb022f77213a675b6aa69567c22e7
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-03 12:49:59 -0600

    Rutas de empleado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddEmpleadoComponent } from './empleado/add-empleado/add-empleado.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { EditEmpleadoComponent } from './empleado/edit-empleado/edit-empleado.component';
 
 const routes: Routes = [
+  {path: '',  component: EmpleadoComponent},
   { path: 'addEmpleado', component: AddEmpleadoComponent },
+  { path: 'editEmpleado/:DPI', component: EditEmpleadoComponent },
+  {path: '',  redirectTo : '/', pathMatch:'full'},
 ];
 
 @NgModule({
